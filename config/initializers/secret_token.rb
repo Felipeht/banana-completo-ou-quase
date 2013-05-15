4427385e23bd20e9d69ca799c2e06f4a9bb24e6d
Mi Che Li Lee <mi.lee@codus.com.br>
2013-05-14 21:11:21 -0300

    criacao da estrutura do projeto base do livro

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teste::Application.config.secret_token = '05899bbaa2288994a7933f8a4c279b1acb6436bf62b5fb7c27568834453cb2a43bf691c60da4c7e772a71389a67083f343bbcfc93c6a4528cd9c886eec8f3faf'
